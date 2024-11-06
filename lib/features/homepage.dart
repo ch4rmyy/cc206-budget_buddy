d75067dd0bac87bc6e5cbf73b63c3baf3eb2e13f
Charmie <charmie.continente@wvsu.edu.ph>
2024-11-06 23:23:51 +0800

    Dialog Box Edited

---
+++ lib/features/homepage.dart
@@ -0,0 +1,47 @@
+import 'package:cc206_budget_buddy/features/sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Homepage"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.home,
+              size: 100,
+              color: Colors.green,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Welcome to the Homepage!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "This is a sample screen.",
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> const SignUpPage())); // Navigate back to the previous screen
+              },
+              child: const Text("Back to Signup"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
