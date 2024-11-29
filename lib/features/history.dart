dfcb7803c59024f571b725d154135a02dda561f0
Charmie <charmie.continente@wvsu.edu.ph>
2024-11-29 17:15:05 +0800

    layout wala pa design

---
+++ lib/features/history.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:very_good_infinite_list/very_good_infinite_list.dart';
+
+class History extends StatefulWidget {
+  @override
+  _HistoryState createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  var _items = <String>[];
+  var _isLoading = false;
+
+  void _fetchData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = false;
+      _items = List.generate(_items.length + 10, (i) => 'Item $i');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Example'),
+      ),
+      body: InfiniteList(
+        itemCount: _items.length,
+        isLoading: _isLoading,
+        onFetchData: _fetchData,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            dense: true,
+            title: Text(_items[index]),
+          );
+        },
+      ),
+    );
+  }
+}
